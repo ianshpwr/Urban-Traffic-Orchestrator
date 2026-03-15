03181b2edf7c86f36c0a57e83c96fa95e7870c46
Ansh <ianshpwr@gmail.com>
2026-03-15 22:51:56 +0530

    fixed the live logging of traffic simulation

--- collectors/traffic_collector.py
+++ collectors/traffic_collector.py
@@ -1,22 +1,34 @@
+import os
+import sys
 import traci
 import csv
 
-sumoCmd = ["sumo-gui", "-c", "../simulation/run.sumo.cfg"]
+# Locate SUMO
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("Please declare environment variable 'SUMO_HOME'")
 
+sumoBinary = "sumo-gui"
+sumoCmd = ["sumo-gui", "-c", "simulation/run.sumo.cfg", "--start", "--quit-on-end"]
+
+# Start simulation
 traci.start(sumoCmd)
 
-log_file = open("../logs/traffic_events.csv","w",newline="")
+log_file = open("logs/traffic_events.csv", "w", newline="")
 writer = csv.writer(log_file)
-
 writer.writerow(["time","vehicle","speed","waiting_time"])
 
 while traci.simulation.getMinExpectedNumber() > 0:
 
     traci.simulationStep()
+
     vehicles = traci.vehicle.getIDList()
 
-    for v in vehicles:
+    print("Vehicles:", len(vehicles))
 
+    for v in vehicles:
         speed = traci.vehicle.getSpeed(v)
         wait = traci.vehicle.getWaitingTime(v)
 
@@ -26,6 +38,6 @@ while traci.simulation.getMinExpectedNumber() > 0:
             speed,
             wait
         ])
-
+        
 traci.close()
 log_file.close()
